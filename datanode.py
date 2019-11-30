6e67b54e16334b0d0ac7b85adf52a87ab288ac6c
Onbehalfofme <33292918+Onbehalfofme@users.noreply.github.com>
2019-11-30 20:31:59 +0300

    Add files via upload

---
+++ datanode.py
@@ -0,0 +1,178 @@
+import json
+import math
+import os
+from random import randint
+import shutil
+
+import requests
+from flask import Flask, request, Response
+from flask_cors import CORS
+
+app = Flask(__name__)
+CORS(app)
+file_names = {}
+files = []
+free_index = 0
+namenode = '10.91.86.17:5000'
+
+
+def create_dir(path):
+    paths = path.split('/')
+    current_path = '/'
+    for p in paths[:-1]:
+        current_path += p
+        if not os.path.exists(current_path):
+            os.mkdir(current_path)
+
+
+@app.route('/upload', methods=['POST'])
+def download_file():
+    path = request.headers.get('File-Name', type=str)
+    file_id = request.headers.get('File-Id', type=int)
+    chunk_id = request.headers.get('Chunk-Id', type=int)
+    chunk_length = request.headers.get('Chunk-Number', type=int)
+    if file_names.get(file_id) is None:
+        file_names.update({file_id: (len(files), path)})
+        current_index = len(files)
+        files.append([None for _ in range(chunk_length)])
+    else:
+        current_index = file_names.get(file_id)[0]
+
+    files[current_index][chunk_id] = request.data
+
+    if None not in files[current_index]:
+        replications = request.headers.get('Replications', type=int)
+        address = None
+        if replications:
+            replications += 1
+        else:
+            replications = 1
+        if replications < 3:
+            r = requests.get(url='http://' + namenode + '/replicate', headers={'path': path})
+            address = r.headers.get('address')
+        file = bytearray()
+        index = 0
+        for chunk in files[current_index]:
+            file.extend(bytearray(chunk))
+            if address:
+                headers = dict(request.headers)
+                headers.update({'Replications': str(replications + 1), 'Chunk-Id': str(index)})
+                requests.post(url='http://' + address + '/upload', headers=headers, data=chunk)
+                if index > len(files[current_index]):
+                    index = 0
+                else:
+                    index += 1
+        create_dir(path)
+        with open(path, 'wb') as fp:
+            fp.write(file)
+        del files[current_index]
+        del file_names[file_id]
+        for d in file_names:
+            if d[0] > current_index:
+                file_names.update({d[0], (d[1][0] - 1, d[1][1])})
+
+    return Response(status=200)
+
+
+@app.route('/create', methods=['GET'])
+def create_file():
+    path = request.headers.get('File-Name', type=str)
+    create_dir(path)
+    replications = request.headers.get('Replications', type=int)
+    address = None
+    if replications:
+        replications += 1
+    else:
+        replications = 1
+    if replications <= 3:
+        r = requests.get(url='http://' + namenode + '/replicate', headers={'path': path})
+        address = r.headers.get('address')
+    open(path, 'a').close()
+    if address:
+        headers = dict(request.headers)
+        headers.update({'Replications': str(replications + 1)})
+        requests.post(url='http://' + address + '/upload', headers=headers)
+    return Response(200)
+
+
+@app.route('/download', methods=['GET'])
+def upload_file():
+    path = request.headers.get('File-Name', type=str)
+    with open(path, 'rb') as fp:
+        data = fp.read()
+    return Response(status=200, response=data)
+
+
+@app.route('/health', methods=['GET'])
+def health():
+    global namenode
+    namenode = 'http://' + request.remote_addr
+    return Response(status=200)
+
+
+@app.route('/delete', methods=['GET'])
+def delete():
+    path = request.headers.get('path')
+    os.remove(path)
+    return Response(status=200)
+
+
+@app.route('/rmdir')
+def rmdir():
+    path = request.headers.get('path')
+    os.system('rm -rf ' + path)
+    return Response(status=200)
+
+
+@app.route('/info', methods=['GET'])
+def file_info():
+    path = request.headers.get('path')
+    st = os.stat(path)
+    data = {'size': str(st.st_size), 'last_accessed': str(st.st_atime), 'last_modified': str(st.st_mtime)}
+    return Response(status=200, response=json.dumps(data))
+
+
+@app.route('/copy', methods=['GET'])
+def copy():
+    old_path = request.headers.get('File-Name-Old')
+    new_path = request.headers.get('File-Name-New')
+    create_dir(new_path)
+    os.system('sudo cp ' + old_path + ' ' + new_path)
+    return Response(200)
+
+
+@app.route('/init', methods=['get'])
+def init():
+    os.system('rm -rf ~/PycharmProjects/DFS/static')
+    total, used, free = shutil.disk_usage('/')
+    return Response(status=200, response=str(free).encode())
+
+
+@app.route('/replicate', methods=['GET'])
+def replicate():
+    path = request.headers.get('path')
+    address = request.headers.get('address')
+    with open(path, 'rb') as fp:
+        chunk = fp.read(1024)
+        chunk_id = 0
+        file_id = randint(0, 1000)
+        chunk_number = math.ceil(os.path.getsize(path) / 1024)
+        while chunk:
+            requests.post('http://' + address + '/upload', data=chunk,
+                          headers={'File-Name': path, 'File-Id': file_id, 'Chunk-Id': chunk_id,
+                                   'Chunk-Number': chunk_number, 'Replications': 3})
+            chunk_id += 1
+    return Response(200)
+
+
+@app.route('/move', methods=['GET'])
+def move():
+    old_path = request.headers.get('File-Name-Old')
+    new_path = request.headers.get('File-Name-New')
+    create_dir(new_path)
+    os.system('sudo mv ' + old_path + ' ' + new_path)
+    return Response(200)
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0')
