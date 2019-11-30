eb8d5cf96cd12101e1854f93819e8db5ed55c6dd
lackadaisicalcynic <35664793+lackadaisicalcynic@users.noreply.github.com>
2019-11-30 23:05:20 +0300

    Add files via upload

---
+++ FileTree.py
@@ -0,0 +1,162 @@
+import json
+import random
+import requests
+from flask import Flask, Response
+
+
+class FileTree:
+    class Node:
+        def __init__(self, name: str, is_dir: bool, parent=None, children: dict = {},
+                     location: list = None):
+            self.children = children
+            self.parent = parent
+            self.name = name
+            self.is_dir = is_dir
+            self.location = location
+
+    def __init__(self):
+        self.root = FileTree.Node('/', True, children={})
+
+    def add_node(self, path: str, is_dir: bool, location: list):
+        directories = path.split('/')[1:]
+        current_node = self.root
+        for dir in directories[:-1]:
+            current_node = current_node.children.get(dir)
+        current_node.children.update({directories[-1]: FileTree.Node(directories[-1], is_dir, parent=current_node, children={}, location=location)})
+
+    def update_location(self, path: str, location: str):
+        paths = path.split('/')[1:]
+        current_node = self.root
+        for p in paths:
+            current_node = current_node.children.get(p)
+        current_node.location.append(location)
+
+    def search_node(self, path: str):
+        directories = path.split("/")[1:]
+        current_node = self.root
+        for dir in directories:
+            current_node = current_node.children.get(dir)
+        if current_node != None:
+            return current_node.location
+        return []
+
+
+    def delete_node(self, path: str, all_datanodes):
+        directories = path.split("/")[1:]
+        current_node = self.root
+        for dir in directories:
+            current_node = current_node.children.get(dir)
+        if current_node != None:
+            if not current_node.is_dir:
+                datanodes = current_node.location
+                for dn in datanodes:
+                    request_delete(dn, path, current_node.is_dir)
+            else:
+                for dn in all_datanodes:
+                    request_delete(dn, path, current_node.is_dir)
+            current_node.parent.children.pop(current_node.name)
+            return Response(200)
+        else:
+            return Response(status=400, response="doesn`t exist")
+
+    def info_node(self, path: str):
+        directories = path.split("/")[1:]
+        current_node = self.root
+        for dir in directories:
+            current_node = current_node.children.get(dir)
+        if current_node != None:
+            return request_info(current_node.location[0], path)
+        else:
+            return {}
+
+    def directory_exists(self, path: str):
+        directories = path.split("/")[1:]
+        current_node = self.root
+        for dir in directories:
+            current_node = current_node.children.get(dir)
+        if current_node != None and current_node.is_dir == True:
+            return True
+        else:
+            return False
+
+    def file_exists(self, path: str):
+        directories = path.split("/")[1:]
+        current_node = self.root
+        for dir in directories:
+            current_node = current_node.children.get(dir)
+        if current_node != None and current_node.is_dir == False:
+            return True
+        else:
+            return False
+
+    def ls(self, path: str):
+        if path == "/":
+            result = {}
+            for node in self.root.children:
+                name = node
+                d = self.root.children[node].is_dir
+                result.__setitem__(name, d)
+            return result
+        directories = path.split("/")[1:]
+        current_node = self.root
+        for dir in directories:
+            current_node = current_node.children.get(dir)
+        if current_node != None:
+            result = {}
+            for node in current_node.children:
+                name = node
+                d = current_node.children[node].is_dir
+                result.__setitem__(name, d)
+            return result
+        else:
+            return None
+
+    def recursive_recover(self, current_level, datanode):
+        # current_level = self.root
+        for node in current_level.children:
+            current_node = current_level.children.get(node)
+            if current_node.is_dir:
+                self.recursive_recover(current_node, datanode)
+            else:
+                replica_servers = current_node.location
+                if datanode in replica_servers:
+                    replica_servers.remove(datanode)
+                    if not replica_servers == []:
+                        try:
+                            requests.get("http://" + random.choice(replica_servers) + "/replicate",
+                                         headers={'address': datanode})
+                        except requests.exceptions.RequestException:
+                            pass
+
+
+def request_delete(datanode, path, is_dir):
+    if is_dir:
+        try:
+            response = requests.get("http://" + datanode + "/rmdir", headers={'path': path})
+        except requests.exceptions.RequestException:
+            return Response(status=500)
+
+    else:
+        try:
+            response = requests.get("http://" + datanode + "/delete", headers={'path': path})
+        except requests.exceptions.RequestException:
+            return Response(status=500)
+
+    return response
+
+
+def request_info(datanode, path):
+    try:
+        response = requests.get("http://" + datanode + "/info", headers={'path': path})
+    except requests.exceptions.RequestException:
+        return Response(status=500)
+
+    return response
+
+def recover(source, target, path):
+    try:
+        response = requests.get("http://" + source + "/replicate", headers={'address': target, 'path': path})
+    except requests.exceptions.RequestException:
+        return Response(status=500)
+
+    return response
---
+++ namenode.py
@@ -0,0 +1,208 @@
+from flask import Flask, Response, request
+import requests
+import random
+from apscheduler.schedulers.background import BackgroundScheduler
+from FileTree import FileTree
+from flask_cors import CORS
+import json
+app = Flask(__name__)
+CORS(app)
+
+
+@app.route('/')
+def hello_world():
+    return 'Hello World!'
+
+
+def find_datanodes():
+    subnet = '127.31.0.'
+    for i in range(256):
+        ip = subnet + str(i) + ":5000"
+        try:
+            requests.get("http://" + ip + "/health", timeout=5)
+        except requests.exceptions.RequestException:
+            continue
+
+        datanodes.append(ip)
+        print(ip + " success")
+
+
+def heartbeat():
+    for node in datanodes:
+        try:
+            requests.get("http://" + node + "/health", timeout=5)
+            if deadnodes.__contains__(node):
+                synchronize(node)
+                deadnodes.remove(node)
+        except requests.exceptions.RequestException:
+            if not deadnodes.__contains__(node):
+                deadnodes.append(node)
+
+
+def synchronize(node):
+    requests.get("http://" + node + "/init")
+    file_tree.recursive_recover(file_tree.root, node)
+
+@app.route('/init')
+def init():
+    global file_tree
+    file_tree = FileTree()
+    result = 0
+
+    for datanode in datanodes:
+        try:
+            response = requests.get("http://" + datanode + "/init")
+        except requests.exceptions.RequestException:
+            pass
+        result += int(response._content.decode())
+    return Response(status=200, response=str(result // 3))
+
+
+@app.route('/create')
+def create():
+    file_path = request.headers.get('File-Name', type=str)
+    datanode = random.choice(datanodes)
+    while datanode not in deadnodes:
+      datanode = random.choice(datanodes)
+    # datanode = '10.91.91.190:5000'
+    file_tree.add_node(file_path, False, list([datanode]))
+    try:
+        requests.get("http://" + datanode + "/create", headers={'File-Name': file_path})
+    except requests.exceptions.RequestException:
+        pass
+    return Response(status=200)
+
+
+@app.route('/read')
+def read():
+    file_path = request.headers.get('File-Name', type=str)
+    nodes = file_tree.search_node(file_path)
+    if nodes != []:
+        return Response(status=200, response=json.dumps(nodes))
+    return Response(status=400)
+
+
+@app.route('/write')
+def write():
+    file_path = request.headers.get('File-Name', type=str)
+    datanode = random.choice(datanodes)
+    while datanode not in deadnodes:
+      datanode = random.choice(datanodes)
+    # datanode = '10.91.91.190:5000'
+    file_tree.add_node(file_path, False, list([datanode]))
+    return Response(status=200, response=datanode)
+
+
+@app.route('/delete_file')
+def delete_file():
+    file_path = request.headers.get('File-Name', type=str)
+    if not file_tree.file_exists(file_path):
+        return Response(status=400, response="file doesn`t exist")
+    return file_tree.delete_node(file_path, datanodes)
+
+
+@app.route('/info')
+def info():
+    file_path = request.headers.get('File-Name', type=str)
+    result = file_tree.info_node(file_path)
+    return Response(status=200, response=json.dumps(result.json()))
+
+
+@app.route('/copy')
+def copy():
+    file_path = request.headers.get('File-Name-Old', type=str)
+    new_path = request.headers.get('File-Name-New', type=str)
+    nodes = file_tree.search_node(file_path)
+    if file_tree.search_node(new_path) != []:
+        return Response(status=400)
+    if file_tree.search_node(file_path) == []:
+        return Response(status=400)
+    nodes = file_tree.search_node(file_path)
+    file_tree.add_node(new_path, False, nodes)
+    for node in nodes:
+        try:
+            requests.get("http://" + node + "/copy", headers={'File-Name-Old': file_path, 'File-Name-New': new_path})
+        except requests.exceptions.RequestException:
+            pass
+
+    return Response(status=200)
+
+
+@app.route('/replicate')
+def replicate():
+    file_path = request.headers.get('path', type=str)
+    occupied = file_tree.search_node(file_path)
+    for dn in datanodes:
+        if dn not in occupied and dn not in deadnodes:
+            file_tree.update_location(file_path, dn)
+            return Response(status=200, headers={"address": dn})
+    return Response(status=200)
+
+
+@app.route('/move')
+def move():
+    file_path = request.headers.get('File-Name-Old', type=str)
+    new_path = request.headers.get('File-Name-New', type=str)
+    if file_tree.search_node(new_path) != []:
+        return Response(status=400)
+    if file_tree.search_node(file_path) == []:
+        return Response(status=400)
+    nodes = file_tree.search_node(file_path)
+    file_tree.add_node(new_path, False, nodes)
+    for node in nodes:
+        try:
+            requests.get("http://" + node + "/move", headers={'File-Name-Old': file_path, 'File-Name-New': new_path})
+        except requests.exceptions.RequestException:
+            pass
+
+    file_tree.delete_node(file_path, datanodes)
+    return Response(status=200)
+
+
+@app.route('/cd')
+def cd():
+    directory = request.headers.get('Directory', type=str)
+    if file_tree.directory_exists(directory) or directory == '/':
+        return Response(status=200)
+    else:
+        return Response(status=400)
+
+
+@app.route('/ls')
+def ls():
+    directory = request.headers.get('Directory', type=str)
+    result = file_tree.ls(directory)
+    if result == None:
+        return Response(status=400)
+    return Response(status=200, response=json.dumps(result))
+
+
+@app.route('/mkdir')
+def mkdir():
+    directory = request.headers.get('Directory', type=str)
+    file_tree.add_node(directory, True, datanodes)
+    return Response(status=200)
+
+
+@app.route('/delete_dir')
+def delete_dir():
+    directory = request.headers.get('Directory', type=str)
+    if not file_tree.directory_exists(directory):
+        return Response(status=400, response="directory doesn`t exist")
+    file_tree.delete_node(directory, datanodes)
+    return Response(status=200)
+
+
+if __name__ == '__main__':
+    file_tree = FileTree()
+    # datanodes = ['10.91.91.190:5000', '10.91.91.190:5001']
+    datanodes = []
+    deadnodes = []
+    scheduler = BackgroundScheduler()
+    replication_factor = 3
+    job = scheduler.add_job(find_datanodes, 'interval', seconds=300)
+    scheduler.start()
+    job = scheduler.add_job(heartbeat, 'interval', seconds=30)
+    scheduler.start()
+    app.run(host='0.0.0.0', port=80, debug=True, use_reloader=False)
+
